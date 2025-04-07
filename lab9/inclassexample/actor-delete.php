4286169d36987556d90dc30e4c707f8e0037d4e4
Pablo Semidey <semideypablo@gmail.com>
2025-04-07 00:10:51 -0400

    linked delete file

--- lab9/inclassexample/actor-delete.php
+++ lab9/inclassexample/actor-delete.php
@@ -3,7 +3,7 @@
   
   /* Create a new database connection object, passing in the host, username,
      password, and database to use. The "@" suppresses errors. */
-  @ $db = new mysqli('localhost', 'root', 'root', 'iit');
+     @$db = new mysqli('localhost', 'phpmyadmin', 'Antonio00!1074', 'iit');
   
   if ($db->connect_error) {
     $connectErrors = array(
