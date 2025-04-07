d0294fba24a6ba0e8a261cb26e668c712ba34b43
Pablo Semidey <semideypablo@gmail.com>
2025-04-07 18:00:50 -0400

    updated actors?movies

--- lab9/movie_actors.sql
+++ lab9/movie_actors.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `movie_actors` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `last_name` VARCHAR(100) NOT NULL,
-  `movie_title` VARCHAR(255) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  UNIQUE KEY `unique_relationship` (`last_name`, `title`),
   INDEX `idx_last_name` (`last_name`),
-  INDEX `idx_movie` (`movie_title`)
+  INDEX `idx_movie` (`title`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
