b770ec0a5d5a42bb18ba1fa9c47e31cbcdb9632f
Pablo Semidey <semideypablo@gmail.com>
2025-04-07 15:37:15 -0400

    update movie_actors

--- lab9/movie_actors.sql
+++ lab9/movie_actors.sql
@@ -3,7 +3,17 @@ CREATE TABLE IF NOT EXISTS `movie_actors` (
   `first_names` VARCHAR(100) NOT NULL,
   `last_name` VARCHAR(100) NOT NULL,
   `movie_title` VARCHAR(255) NOT NULL,
-  `year` INT,
   INDEX `idx_names` (`last_name`, `first_names`),
   INDEX `idx_movie` (`movie_title`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Sample data
+INSERT INTO `movie_actors` (`first_names`, `last_name`, `movie_title`) VALUES
+('Chris', 'Hemsworth', 'Elizabeth'),
+('Scarlett', 'Johansson', 'Black Widow'),
+('George', 'Clooney', 'Oh Brother Where Art Thou?'),
+('Elijah', 'Wood', 'The Lord of the Rings: The Fellowship of the Ring'),
+('Anna', 'Kendrick', 'Up in the Air'),
+('Tom', 'Hanks', 'The Godfather'),
+('Robert', 'Duvall', 'The Godfather'),
+('Morgan', 'Freeman', 'The Godfather');
