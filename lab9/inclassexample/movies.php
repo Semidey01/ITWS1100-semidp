a14f40bd43c0decb1f060275c05974f6fb4b6f61
Pablo Semidey <semideypablo@gmail.com>
2025-04-07 13:26:16 -0400

    Update movies.php

--- lab9/inclassexample/movies.php
+++ lab9/inclassexample/movies.php
@@ -1,49 +1,51 @@
-<?php
-include('includes/init.inc.php');
-include('includes/functions.inc.php');
+<?php 
+  include('includes/init.inc.php'); // include the DOCTYPE and opening tags
+  include('includes/functions.inc.php'); // functions
+  error_reporting(E_ALL);
+  ini_set('display_errors', '1');
 ?>
-<title>PHP &amp; MySQL - ITWS</title>
+<title>PHP &amp; MySQL - ITWS</title>   
 
-<?php
-include('includes/head.inc.php');
-?>
+<?php include('includes/head.inc.php'); ?>
 
 <h1>PHP &amp; MySQL</h1>
-
 <?php include('includes/menubody.inc.php'); ?>
 
 <?php
-$dbOk = false;
-@$db = new mysqli('localhost', 'phpmyadmin', 'Antonio00!1074', 'iit');
+  $dbOk = false;
+  
 
-if ($db->connect_error) {
-   echo '<div class="messages">Could not connect to the database. Error: ';
-   echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
-} else {
-   $dbOk = true;
-}
+  @ $db = new mysqli('localhost', 'phpmyadmin', 'password', 'iit');
+  
+  if ($db->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+  } else {
+    $dbOk = true;
+  }
 
-$havePost = isset($_POST["save"]);
-$errors = '';
-if ($havePost) {
-   $title = htmlspecialchars(trim($_POST["title"]));
-   $year = htmlspecialchars(trim($_POST["year"]));
+  $havePost = isset($_POST["save"]);
 
-   $focusId = '';
-   if ($title == '') {
-      $errors .= '<li>Title may not be blank</li>';
+  $errors = '';
+  if ($havePost) {
+
+    $movieTitle = htmlspecialchars(trim($_POST["title"]));
+    $movieYear = htmlspecialchars(trim($_POST["year"]));
+
+    $focusId = ''; // trap the first field that needs updating, better would be to save errors in an array
+
+    if ($movieTitle == '') {
+      $errors .= '<li>Title name may not be blank</li>';
       if ($focusId == '') $focusId = '#title';
-   }
-   if ($year == '') {
+    }
+
+    if ($movieYear == '') {
       $errors .= '<li>Year may not be blank</li>';
       if ($focusId == '') $focusId = '#year';
-   }
-   if (!is_numeric($year)) {
-      $errors .= '<li>Year must be a number</li>';
-      if ($focusId == '') $focusId = '#year';
-   }
+    }
 
-   if ($errors != '') {
+
+    if ($errors != '') {
       echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
       echo $errors;
       echo '</ul></div>';
@@ -52,72 +54,73 @@ if ($havePost) {
       echo '    $("' . $focusId . '").focus();';
       echo '  });';
       echo '</script>';
-   } else {
+    } else {
       if ($dbOk) {
-         $titleForDb = trim($_POST["title"]);
-         $yearForDb = trim($_POST["year"]);
+        $movieTitleForDb = trim($_POST["title"]);
+        $movieYearForDb = trim($_POST["year"]);
 
-         $insQuery = "insert into movies (title, year) values(?,?)";
-         $statement = $db->prepare($insQuery);
-         $statement->bind_param("si", $titleForDb, $yearForDb);
-         $statement->execute();
 
-         echo '<div class="messages"><h4>Success: ' . $statement->affected_rows . ' movie added to database.</h4>';
-         echo $title . ' (' . $year . ')</div>';
+        $insQuery = "insert into movies (`title`,`year`) values(?,?)";
+        $statement = $db->prepare($insQuery);
+        // bind our variables to the question marks
+        $statement->bind_param("ss",$movieTitleForDb,$movieYearForDb);
+        $statement->execute();
 
-         $statement->close();
+        echo '<div class="messages"><h4>Success: ' . $statement->affected_rows . ' movie added to database.</h4>';
+        echo $movieTitle . ' ' . $movieYear . '</div>';
+
+        $statement->close();
       }
-   }
-}
+    }
+  }
 ?>
 
 <h3>Add Movie</h3>
-<form id="addForm" name="addForm" action="movies.php" method="post" onsubmit="return validateMovie(this);">
-   <fieldset>
-      <div class="formData">
-         <label class="field" for="title">Title:</label>
-         <div class="value"><input type="text" size="60" value="<?php if ($havePost && $errors != '') {
-                                                                     echo $title;
-                                                                  } ?>" name="title" id="title" /></div>
+<form id="addForm" name="addForm" action="movies.php" method="post" onsubmit="return validate(this);">
+  <fieldset>
+    <div class="formData">
 
-         <label class="field" for="year">Year:</label>
-         <div class="value"><input type="text" size="4" maxlength="4" value="<?php if ($havePost && $errors != '') {
-                                                                                    echo $year;
-                                                                                 } ?>" name="year" id="year" /></div>
+      <label class="field" for="title">Movie Title:</label>
+      <div class="value"><input type="text" size="60" value="<?php if($havePost && $errors != '') { echo $movieTitle; } ?>" name="title" id="title"/></div>
 
-         <input type="submit" value="save" id="save" name="save" />
-      </div>
-   </fieldset>
+      <label class="field" for="year">Movie Year:</label>
+      <div class="value"><input type="text" size="60" value="<?php if($havePost && $errors != '') { echo $movieYear; } ?>" name="year" id="year"/></div>
+
+      <input type="submit" value="save" id="save" name="save"/>
+    </div>
+  </fieldset>
 </form>
 
 <h3>Movies</h3>
 <table id="movieTable">
-   <?php
-   if ($dbOk) {
-      $query = 'select * from movies order by year';
-      $result = $db->query($query);
-      $numRecords = $result->num_rows;
+<?php
+  if ($dbOk) {
 
-      echo '<tr><th>Title:</th><th>Year:</th><th></th></tr>';
-      for ($i = 0; $i < $numRecords; $i++) {
-         $record = $result->fetch_assoc();
-         if ($i % 2 == 0) {
-            echo "\n" . '<tr id="movie-' . $record['movieid'] . '"><td>';
-         } else {
-            echo "\n" . '<tr class="odd" id="movie-' . $record['movieid'] . '"><td>';
-         }
-         echo htmlspecialchars($record['title']);
-         echo '</td><td>';
-         echo htmlspecialchars($record['year']);
-         echo '</td><td>';
-         echo '<img src="resources/delete.png" class="deleteMovie" width="16" height="16" alt="delete movie"/>';
-         echo '</td></tr>';
+    $query = 'select * from movies order by year';
+    $result = $db->query($query);
+    $numRecords = $result->num_rows;
+
+    echo '<tr><th>Title:</th><th>Year:</th><th></th></tr>';
+    for ($i=0; $i < $numRecords; $i++) {
+      $record = $result->fetch_assoc();
+      if ($i % 2 == 0) {
+        echo "\n".'<tr id="movie-' . $record['movieid'] . '"><td>';
+      } else {
+        echo "\n".'<tr class="odd" id="movie-' . $record['movieid'] . '"><td>';
       }
+      echo htmlspecialchars($record['title']);
+      echo '</td><td>';
+      echo htmlspecialchars($record['year']);
+      echo '</td></tr>';
 
-      $result->free();
-      $db->close();
-   }
-   ?>
+    }
+
+    $result->free();
+
+    $db->close();
+  }
+
+?>
 </table>
-
-<?php include('includes/foot.inc.php'); ?>
+<?php include('includes/foot.inc.php'); 
+?>
