4d63737919dee5dd63a626fd8d27bc6de487cde6
Pablo Semidey <semideypablo@gmail.com>
2025-04-21 18:52:58 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="Lab3/css/top.css">
+        <link rel="stylesheet" href="Lab3/css/homepage.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Iceberg&display=swap" rel="stylesheet">
+        <title>Pablo Semidey Website</title>
+    </head>
+    <body>
+        <?php include 'quiz3/menu.php'; ?>
+        <?php echo buildMenu(); ?>
+
+        <!-- name and background image-->
+        <section class="heading">
+            <div class="name">
+                <h1>Pablo Semidey</h1>
+            </div>
+        </section>
+
+    </body>
+</html>
